7ce990a4599beadd45448405924b9aabf7531e66
Peter Karth <falonofthetower@gmail.com>
2015-07-12 04:06:44 +0000

    better syntax for validations

--- app/models/giver.rb
+++ app/models/giver.rb
@@ -1,7 +1,7 @@
 class Giver < ActiveRecord::Base
   include Tokenable
 
-  validates_presence_of :full_name, :message, :email
+  validates :full_name, :message, :email, presence: true
   validates :email, uniqueness: true
   has_secure_password validations: false
 end
