9941b25244ab5e73918a581dad990c826404f351
Peter Karth <falonofthetower@gmail.com>
2015-07-12 04:08:49 +0000

    fixing 's

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "givers#new"
   resources :givers, only: [:new, :create, :show]
   resources :access_accounts, only: [:new, :create, :show]
-  get '/token_sessions/:token' => 'token_sessions#create', as: :token_sessions
+  get "/token_sessions/:token" => "token_sessions#create", as: :token_sessions
 end
 
 Donor::Application.routes.draw do
