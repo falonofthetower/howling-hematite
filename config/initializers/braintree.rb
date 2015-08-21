778f4e9cdc37eb57721429bda819768854758919
Peter Karth <falonofthetower@gmail.com>
2015-08-21 17:28:03 -0400

    removed exposed braintree variables

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
 Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = "svwhj9sfmh38gwmk"
-Braintree::Configuration.public_key = "22w3shf6cs8c95y5"
-Braintree::Configuration.private_key = "cb5a46b6b943a52eac442c6f306b0d19"
+Braintree::Configuration.merchant_id = ENV["BRAINTREE_MERCHANT_ID"]
+Braintree::Configuration.public_key = ENV["BRAINTREE_PUBLIC_KEY"]
+Braintree::Configuration.private_key = ENV["BRAINTREE_PRIVATE_KEY"]
