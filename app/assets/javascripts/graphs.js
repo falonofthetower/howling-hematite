c1cabc9fdf4499149270a34d210b2df0450a45ac
ikarth <isaac@isaackarth.com>
2015-08-23 20:15:04 +0000

    Input form validation and warning message styling

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -31,17 +31,32 @@ function displayDonations() {
                                 .getElementsByTagName("total")
                                 .item(0)
                                 .textContent)
-                   + 89578942515.2; // add this to total for testing
+                   + 9578942515.2; // add this to total for testing
+    var donorTotal = Number(data.documentElement
+                                .getElementsByTagName("total_number_of_donors")
+                                .item(0)
+                                .textContent)
+    + 4568;
 
     d3.selectAll(".donation-amount")
-    .text("")
-    .transition()
-    .duration(4000)
+      .text("")
+      .transition()
+      .duration(4000)
       .tween("text", function(d) {
         var last = 0;
         var i = d3.interpolate(last, data_total);
-      console.log(data_total);
-      return function(t) { this.textContent = toCurrencyString(i(t))};
-    });
+        return function(t) { this.textContent = toCurrencyString(i(t))};
+      });
+    
+    d3.selectAll(".donation-donors")
+      .text("")
+      .transition()
+      .duration(4000)
+      .tween("text", function(d) {
+        var last = 0;
+        var i = d3.interpolate(last, donorTotal);
+        return function(t) { this.textContent = Math.round(i(t))};
+      });
   });
 };
+
