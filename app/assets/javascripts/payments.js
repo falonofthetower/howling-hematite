06f081b5a0a0e0d6fb3401cb3aefa015e5306859
ikarth <isaac@isaackarth.com>
2015-09-07 19:59:15 +0000

    Formatted input form

--- app/assets/javascripts/payments.js
+++ app/assets/javascripts/payments.js
@@ -3,13 +3,27 @@ function braintree_setup() {
     id: "payment-form",
     hostedFields: {
       number: {
-        selector: "#card-number"
+        selector: "#card-number",
+        placeholder: "Card Number"
       },
       cvv: {
-        selector: "#cvv"
+        selector: "#cvv-number",
+        placeholder: "CVV"
       },
       expirationDate: {
-        selector: "#expiration-date"
+        selector: "#expiration-date",
+        placeholder: "MM/YYYY"
+      },
+      styles: {
+        "input": {
+          "font-size:":"36pt",
+          "font-family":"'Courier New', Courier, 'Lucida Sans Typewriter', 'Lucida Typewriter', monospace"
+        },
+        ":focus": {
+          "color":"blue"
+        },
+        "card-number": {"background-color":"blue"}
+
       }
     }
   });
