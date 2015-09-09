265018b362a67dd8fce0a31915af7d7696206667
ikarth <isaac@isaackarth.com>
2015-09-09 14:54:38 +0000

    Added Thank You and updated retractable article handling

--- app/assets/javascripts/retractable.js
+++ app/assets/javascripts/retractable.js
@@ -1,27 +1,44 @@
 function toggleHidden(header) {
   if(header.hasClass("collapsed")) {
-    header.children("section").removeClass("hidden");
+    unhide(header);
+  } else {
+    hide(header);
+  }
+}
+function hide(header) {
+  if(typeof header === "object") {
+    header.children("section").addClass("hidden");
+      if(header.hasClass("all-hidden")) {
+         header.addClass("hidden");
+      }
+      header.addClass("collapsed");
+  }
+}
+function unhide (header) {
+  if(!(typeof header === "object")) { return; }
+  header.children("section").removeClass("hidden");
     if(header.hasClass("all-hidden")) {
        header.removeClass("hidden");
     }
     header.removeClass("collapsed");
-  } else {
-    header.children("section").addClass("hidden");
-    if(header.hasClass("all-hidden")) {
-       header.addClass("hidden");
-    }
-    header.addClass("collapsed");
-  }
 }
 
 $(window).load(function() {
-  $("article.retractable").click(function() { toggleHidden($(this)); });
+  $("article.retractable section h1").hover(
+    function() { 
+      $(this).parent().parent().addClass("hovering"); },
+    function() { 
+      $(this).parent().parent().removeClass("hovering"); 
+    });
+  $("article.retractable h1").click(function() { toggleHidden($(this).parent().parent()); });
   toggleHidden($("#privacy-statement"));
   $("#reveal-privacy").click(function() { toggleHidden($("#privacy-statement")); });
   toggleHidden($("#terms-of-use"));
   $("#reveal-terms-of-use").click(function() { toggleHidden($("#terms-of-use")); });
   toggleHidden($("#disclaimer"));
   $("#reveal-disclaimer").click(function() { toggleHidden($("#disclaimer")); });
-  toggleHidden($(".start-hidden"));
+  toggleHidden($("#contact-us"));
+  $("#reveal-contact-info").click(function() { toggleHidden($("#contact-us")); });
+  hide(".start-hidden");
 
 });
