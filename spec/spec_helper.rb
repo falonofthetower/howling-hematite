212a0fadc41af5bed37fcab61c534c88dc24d41d
Peter Karth <falonofthetower@gmail.com>
2015-09-10 16:17:58 -0400

    added options to poltergeist

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,14 @@ Sidekiq::Testing.inline!
 Capybara.server_port = 52662
 Capybara.javascript_driver = :poltergeist
 
-options = { js_errors: false }
+options = {
+  js_errors: false,
+  timeout: 180,
+  phantomjs_logger: StringIO.new,
+  logger: nil,
+  phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes']
+}
+
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, options)
 end
