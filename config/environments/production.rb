b376510aee9be757ff45a32bcab99804b6e167f0
Peter Karth <falonofthetower@gmail.com>
2015-09-10 15:16:01 -0400

    updated production urls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,12 @@ Rails.application.configure do
     address: ENV["MAILGUN_SMTP_SERVER"],
     user_name: ENV["MAILGUN_SMTP_LOGIN"],
     password: ENV["MAILGUN_SMTP_PASSWORD"],
-    domain: "https://howling-hematite.herokuapp.com/",
+    domain: "//fortrucksafety.com",
     authentication: :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
 
   config.action_mailer.default_url_options = {
-    host: "https://peter-myflix.herokuapp.com/"
+    host: "//fortrucksafety.com"
   }
 end
