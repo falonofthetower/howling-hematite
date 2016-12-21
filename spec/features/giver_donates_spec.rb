d378d6cd4c5d8028c33788239eb608e523418e87
Peter Karth <falonofthetower@gmail.com>
2016-12-21 02:28:27 -0500

    expecting css

--- spec/features/giver_donates_spec.rb
+++ spec/features/giver_donates_spec.rb
@@ -54,13 +54,16 @@ feature "giver donates to cause", :js, :vcr do
 
   def fill_in_valid_card(amount)
     fill_in "Amount", with: amount
-    expect(page).to have_content("Please Help: Donate Here")
-    within_frame("braintree-hosted-field-number") do
+
+    expect(page).to have_css('iframe[name="braintree-hosted-field-number"]')
+    page.driver.within_frame("braintree-hosted-field-number") do
       fill_in "Credit Card Number", with: "4111 1111 1111 1111"
     end
+    expect(page).to have_css('iframe[name="braintree-hosted-field-cvv"]')
     within_frame("braintree-hosted-field-cvv") do
       find(".cvv").set "123"
     end
+    expect(page).to have_css('iframe[name="braintree-hosted-field-expirationDate"]')
     within_frame("braintree-hosted-field-expirationDate") do
       fill_in "Expiration Date", with: "1220"
     end
@@ -68,12 +71,15 @@ feature "giver donates to cause", :js, :vcr do
 
   def fill_in_invalid_card(amount)
     fill_in "Amount", with: amount
+    expect(page).to have_css('iframe[name="braintree-hosted-field-number"]')
     within_frame("braintree-hosted-field-number") do
       fill_in "Credit Card Number", with: "4739 3935 3953 9395"
     end
+    expect(page).to have_css('iframe[name="braintree-hosted-field-cvv"]')
     within_frame("braintree-hosted-field-cvv") do
       find(".cvv").set "123"
     end
+    expect(page).to have_css('iframe[name="braintree-hosted-field-expirationDate"]')
     within_frame("braintree-hosted-field-expirationDate") do
       fill_in "Expiration Date", with: "1220"
     end
