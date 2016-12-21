4ca2c27a85cd0944f8afebd67b795d034f53474a
Peter Karth <falonofthetower@gmail.com>
2016-12-21 11:21:12 -0500

    fixing string issue

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,6 +1,6 @@
 class DonationsController < ApplicationController
   def index
-    @total = Donation.total - 5360
+    @total = (Donation.total.to_f - 5360).to_s
     @giver_count = Giver.unique_count - 30
   end
 end
