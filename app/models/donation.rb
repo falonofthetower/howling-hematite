aeb47ecaec025c0b31a7e6b6446c0abfc464366d
Peter Karth <falonofthetower@gmail.com>
2016-12-23 13:11:38 -0500

    Set donations to return by scope
    - Removed number fudging
    - Filtering out donations lacking datetime
    - Serves as structure to build actual campaigns around

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,8 @@
 class Donation < ActiveRecord::Base
   belongs_to :giver
 
+  scope :current_campaign, -> { where.not('donations.created_at' => nil) }
+
   def self.total
     all.sum(:amount).to_s
   end
