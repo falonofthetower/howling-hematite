cd7e35bb87bddf72e9fd5171b3f0ffc6ddc40916
Peter Karth <falonofthetower@gmail.com>
2019-06-10 12:42:03 -0400

    Only crash map

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,27 @@
 Rails.application.routes.draw do
-  root to: "givers#new"
-  resources :givers, only: [:new, :create, :show, :index]
-  resources :donations, only: [:index]
-  resources :access_accounts, only: [:new, :create, :show]
-  resources :sessions, only: [:new, :create]
-  resources :crash_locations, only: [:index]
-
+  root to: "crash_locations#index"
+#   resources :givers, only: [:new, :create, :show, :index]
+#   resources :donations, only: [:index]
+#   resources :access_accounts, only: [:new, :create, :show]
+   resources :sessions, only: [:new, :create]
+   resources :crash_locations, only: [:index]
+#
   get "/sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
-  get "/thanks" => "givers#thanks", as: :thanks
+#   get "/thanks" => "givers#thanks", as: :thanks
+#
+#   get "ui(/:action)", controller: "ui"
+#
+#   get "/petition" => redirect("http://www.thepetitionsite.com/417/742/234/save-lives-not-dollars-urge-dot-to-adopt-vision-zero-policy/"), as: :petition
 
-  get "ui(/:action)", controller: "ui"
-
-  get "/petition" => redirect("http://www.thepetitionsite.com/417/742/234/save-lives-not-dollars-urge-dot-to-adopt-vision-zero-policy/"), as: :petition
   namespace :admin do
-    resources :gifts, only: [:index]
+    # resources :gifts, only: [:index]
     resources :crash_locations
   end
 end
-
-Donor::Application.routes.draw do
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
-end
+#
+# Donor::Application.routes.draw do
+#   if Rails.env.development?
+#     mount LetterOpenerWeb::Engine, at: "/letter_opener"
+#   end
+# end
